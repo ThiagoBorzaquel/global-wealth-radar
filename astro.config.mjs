f09c21c94fac47aa63df2d326e94fc23f67d2ac5
Thiago <thiago.borzaquel@hotmail.com>
2026-05-21 10:39:37 -0300

    fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+
+
 
 export default defineConfig({
   site: 'https://globalwealthradar.com',
-  base: '/',
+  base: '/global-wealth-radar/',
   output: 'static',
   integrations: [
     tailwind(),
     mdx(),
-    sitemap({
-      filter: (page) =>
-        !page.includes('/premium/') &&
-        !page.includes('/auth/') &&
-        !page.includes('/dashboard'),
-    }),
+    
   ],
   build: { assets: '_assets' },
 });
